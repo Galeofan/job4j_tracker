343788e1e2f495fbae3c713a7c315cf754af3e81
Galeofan <20Andrew12@mail.ru>
2024-11-30 14:11:44 +0300

    0.4. Пользовательские исключения. [#219367 #523943]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -4,25 +4,25 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int result = -1;
         for (int i = 0; i < value.length; i++) {
-            if (value[i] == null) {
-                throw new ElementNotFoundException("Элемент не найден");
-            }
             if (key.equals(value[i])) {
                 result = i;
+                break;
             }
+        }
+        if (result == -1) {
+            throw new ElementNotFoundException("Элемент не найден");
         }
         return result;
     }
 
     public static void main(String[] args) {
         String[] str = new String[2];
-        str[0] = null;
-        str[1] = "1234";
+        str[0] = "12345";
+        str[1] = null;
         try {
             indexOf(str, "1234");
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
-
     }
 }
